5a71a1a28654d4d738f66db8157380f658af4f98
ktafoyam <58603457+ktafoyam@users.noreply.github.com>
2019-12-28 17:18:44 -0700

    Create HomeActivity.java

---
+++ SplashScreenActivity/HomeActivity.java
@@ -0,0 +1,18 @@
+//Katelynn 12-28-19
+
+//This should all already be there when you upload the activity 
+
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
