c9a9d6c7a0f90b7acdba30d88d8a1954d023db04
Katelynn Tafoya-Montoya <58603457+ktafoyam@users.noreply.github.com>
2020-01-08 11:13:38 -0700

    Create PartialReassurance.java

---
+++ Clean Sweep and Save Test Application/PartialReassurance.java
@@ -0,0 +1,3 @@
+//Katelynn Tafoya 01-08-20
+//This is the code for the reassurance that the individual is done deleting stuff. This page will need to be completed once the 
+//Partial Properties is completed. 
