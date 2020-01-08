34789b880dd9e1c83e8606a2728312de470b1c0c
Katelynn Tafoya-Montoya <58603457+ktafoyam@users.noreply.github.com>
2020-01-08 12:08:12 -0700

    Update MainActivity.java

--- Clean Sweep and Save Test Application/MainActivity.java
+++ Clean Sweep and Save Test Application/MainActivity.java
@@ -1,2 +1,28 @@
 //Katelynn Tafoya 01/08/20
 //This is where the code for the Splash Screen Activity Goes. 
+package com.example.cleansweepandsave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler() .postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent welcomeIntent = new Intent(MainActivity.this, WelcomeActivity.class);
+                startActivity(welcomeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
