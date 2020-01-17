dc1d203d8fdf4ce3c2d40ff97727775597d8fd06
krad123 <58607427+krad123@users.noreply.github.com>
2020-01-17 11:25:18 -0700

    button calls

---
+++ DB-Build/MainActivity.java
@@ -0,0 +1,32 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    /**This code below was added automatically by the build**/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    /**Added below for button to be identified by an ID and for the OS to be ready for when a user clicks, then it will open the next screen**/
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                opendbActivity2();
+            }
+        });
+    }
+    /** Called when the user taps the Send button **/
+    public void opendbActivity2(){
+        Intent intent = new Intent(this, dbActivity2.class);
+        startActivity(intent);
+    }
+}
