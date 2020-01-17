9e080bbe86e4f5b2946e7944a38e22dd3738e6be
krad123 <58607427+krad123@users.noreply.github.com>
2020-01-17 11:26:21 -0700

    Creates Next Screen

---
+++ DB-Build/dbActivity2.java
@@ -0,0 +1,13 @@
+package com.hfad.dbtest1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class dbActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_db);
+    }
+}
