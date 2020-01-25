8e286394fcd75b32ea515edc70c6c60f6c5ad8cf
Katelynn Tafoya-Montoya <58603457+ktafoyam@users.noreply.github.com>
2020-01-24 17:00:49 -0700

    Update PartialProperties.java

--- Clean Sweep and Save Test Application/PartialProperties.java
+++ Clean Sweep and Save Test Application/PartialProperties.java
@@ -30,3 +30,53 @@ public class PartialProperties extends AppCompatActivity {
             startActivity(homeIntent);
     }
 }
+
+
+//There was an update to this code that can be found below..... 
+//Katelynn 1/24/20
+//I did not want to completely scratch the code above in case something doesn't work. So, if you would like to try the new code it can 
+// be found below. 
+
+package com.example.cleansweepandsave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PartialProperties extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partial_properties);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWelcomeActivity();
+            }
+        });
+    Button button2 = findViewById(R.id.button2);
+    button2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openContactRemover();
+        }
+    });
+
+    }
+    public void openContactRemover() {
+        Intent removerIntent = new Intent(this, ContactRemover.class);
+        startActivity(removerIntent);
+    }
+
+
+    public void openWelcomeActivity() {
+        Intent homeIntent = new Intent(this, WelcomeActivity.class);
+        startActivity(homeIntent);
+    }
+}
