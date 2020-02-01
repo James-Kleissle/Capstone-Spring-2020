03077674a6a3909bc04f2867fb2859b13ebfd371
krad123 <58607427+krad123@users.noreply.github.com>
2020-02-01 10:10:42 -0700

    WelcomeActivity-Design

---
+++ CleanSweep App-Design/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package com.hfad.cleansweep;
+
+//This is the code for the home screen.
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+
+
+public class WelcomeActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTwoFunctions();
+            }
+        });
+    }
+
+    public void openTwoFunctions() {
+        Intent intent = new Intent (this, TwoFunctions.class);
+        startActivity(intent);
+    }
+}
