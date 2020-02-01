227da676293dc4f0b0b619ff92181798d76d8cb2
krad123 <58607427+krad123@users.noreply.github.com>
2020-02-01 10:06:12 -0700

    PartialReassurance-Design

---
+++ CleanSweep App-Design/PartialReassurance.java
@@ -0,0 +1,16 @@
+package com.hfad.cleansweep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PartialReassurance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partial_reassurance);
+    }
+}
+//This is the code for the reassurance that the individual is done deleting stuff. This page will need to be completed once the
+//Partial Properties is completed. 
