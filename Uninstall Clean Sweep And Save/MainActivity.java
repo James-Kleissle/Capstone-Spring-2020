1f21575e0a669b76049e179879423c17189e962c
Katelynn Tafoya-Montoya <58603457+ktafoyam@users.noreply.github.com>
2020-02-11 15:31:44 -0700

    Create MainActivity.java

---
+++ Uninstall Clean Sweep And Save/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DELETE);
+                intent.setData(Uri.parse("package:com.example.test"));
+                startActivity(intent);
+            }
+        });
+    }
+}
