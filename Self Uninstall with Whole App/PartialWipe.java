1f42e6d0e050c2eed79bfcd9160ce8dfad34fb34
Katelynn Tafoya-Montoya <58603457+ktafoyam@users.noreply.github.com>
2020-02-22 19:31:00 -0700

    Create PartialWipe.java

---
+++ Self Uninstall with Whole App/PartialWipe.java
@@ -0,0 +1,47 @@
+package com.example.remover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PartialWipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partial_wipe);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openPartialProperties();
+            }
+        });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWelcomeActivity();
+            }
+        });
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWelcomeActivity();
+            }
+        });
+    }
+    public void openPartialProperties(){
+        Intent propertiesIntent = new Intent(this, PartialProperties.class);
+        startActivity(propertiesIntent);
+    }
+    public void openWelcomeActivity(){
+        Intent homeIntent = new Intent(this, WelcomeActivity.class);
+        startActivity(homeIntent);
+    }
+}
