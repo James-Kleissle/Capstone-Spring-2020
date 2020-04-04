e23c5f8b355262ec0141173b05e4ca2c17939fa7
Katelynn Tafoya-Montoya <58603457+ktafoyam@users.noreply.github.com>
2020-04-04 16:47:34 -0600

    Create SpanSelfUninstall.java

---
+++ FINAL CLEAN SWEEP AND SAVE/Self Uninstall/SpanSelfUninstall.java
@@ -0,0 +1,28 @@
+package com.example.cleansweepsave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SpanSelfUninstall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_span_self_uninstall);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DELETE);
+                intent.setData(Uri.parse("package:com.example.cleansweepsave"));
+                startActivity(intent);
+            }
+        });
+    }
+}
